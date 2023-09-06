badf5cffcc1c4f5d7ceaba1bc96186e32518ba0a
noumanAhmad127 <ahmadnouman183@gmail.com>
2023-09-07 00:57:21 +0500

    Modified

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// Header
 let menu = document.querySelectorAll(".menu");
 let content = document.querySelectorAll(".content") 
 let menuIcon = document.querySelectorAll(".Micon")
@@ -12,6 +14,20 @@ menu.forEach((li,index)=>{
         menuIcon[index].setAttribute("class","fa-solid fa-chevron-down Micon")
     })
 })
+
+// Home/Index Page
+let slider = document.querySelector(".slider-img")
+let arrbutton = document.querySelectorAll(".slider-button button")
+let firsImgWidth = slider.querySelector(".images").offsetWidth
+
+arrbutton.forEach((btn)=>{
+  btn.addEventListener("click",()=>{
+    slider.scrollLeft += btn.id ==="right" ? -firsImgWidth : firsImgWidth;
+  })
+})
+
+
+// List Item Page
 
 let btns = document.querySelectorAll(".filter-part-list button");
 let lists = document.querySelectorAll(".filter-part-list .filter-lists");
